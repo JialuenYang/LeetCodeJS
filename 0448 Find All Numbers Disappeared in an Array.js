4783d2d022cc2a46e2310debb3a8a04ee490c3b9
Allen <102781876+ClicheCultivator@users.noreply.github.com>
2022-04-22 13:48:45 +0800

    Update 0448 Find All Numbers Disappeared in an Array.js

--- 0448 Find All Numbers Disappeared in an Array.js
+++ 0448 Find All Numbers Disappeared in an Array.js
@@ -3,6 +3,9 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function (nums) {
+	// The go-to solution is probably with a set of nums and just loop from 0 to n and find the missing numbers.
+	// The follow up condition is O(1) space complexity, which means we must use the nums array to keep track of the numbers.
+
 	const output = [];
 	// Modify the array in place with a negative to mark which number we've been through already.
 	for (let i = 0; i < nums.length; i++) {
