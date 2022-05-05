6e716b497b2bb0d35802fecd49eacfcfd5a3c31f
Allen <jialuenyang@gmail.com>
2022-05-05 21:56:28 +0800

    Create 0912. Sort an Array.js

---
+++ 0912. Sort an Array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function (nums) {
+	// Do I look like I have the time to write out an existing sort?
+	// Jokes aside, we shold brush up on sorting techniques in real code.
+	return nums.sort((a, b) => a - b);
+};
