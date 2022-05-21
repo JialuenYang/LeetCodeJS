d6d7a8a6b9e894d8b1df3ab47c48ae64637c4de9
Allen <jialuenyang@gmail.com>
2022-05-21 12:15:17 +0800

    Create 0999. Available Captures for Rook.js

---
+++ 0999. Available Captures for Rook.js
@@ -0,0 +1,64 @@
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+var numRookCaptures = function (board) {
+	// Find coordinates of Rook
+	let Rx = 0;
+	let Ry = 0;
+	loop: for (let i = 0; i < 8; i++) {
+		for (let j = 0; j < 8; j++) {
+			if (board[i][j] === "R") {
+				Rx = i;
+				Ry = j;
+				break loop;
+			}
+		}
+	}
+	// Check left
+	let output = 0;
+	let x = Rx - 1;
+	while (x >= 0) {
+		if (board[x][Ry] === "p") {
+			output++;
+			break;
+		} else if (board[x][Ry] === "B") {
+			break;
+		}
+		x--;
+	}
+	// Check right
+	x = Rx + 1;
+	while (x < 8) {
+		if (board[x][Ry] === "p") {
+			output++;
+			break;
+		} else if (board[x][Ry] === "B") {
+			break;
+		}
+		x++;
+	}
+	// Check up
+	let y = Ry - 1;
+	while (y >= 0) {
+		if (board[Rx][y] === "p") {
+			output++;
+			break;
+		} else if (board[Rx][y] === "B") {
+			break;
+		}
+		y--;
+	}
+	// Check down
+	y = Ry + 1;
+	while (y < 8) {
+		if (board[Rx][y] === "p") {
+			output++;
+			break;
+		} else if (board[Rx][y] === "B") {
+			break;
+		}
+		y++;
+	}
+	return output;
+};
