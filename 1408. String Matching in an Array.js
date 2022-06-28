8c311949898533df89407b7b417d76b41c771dbd
Allen <jialuenyang@gmail.com>
2022-06-28 21:08:27 +0800

    Create 1408. String Matching in an Array.js

---
+++ 1408. String Matching in an Array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var stringMatching = function (words) {
+	const set = new Set();
+	for (let i = 0; i < words.length - 1; i++) {
+		for (let j = i + 1; j < words.length; j++) {
+			if (words[i].length > words[j].length) {
+				if (words[i].includes(words[j])) {
+					set.add(words[j]);
+				}
+			} else {
+				if (words[j].includes(words[i])) {
+					set.add(words[i]);
+				}
+			}
+		}
+	}
+	return [...set];
+};
