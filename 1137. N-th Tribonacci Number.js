8bc3bcb3cd8da29319a3f3d676ebd24b2a75c05c
Allen <jialuenyang@gmail.com>
2022-10-09 21:23:38 -0500

    Problem 1137

---
+++ 1137. N-th Tribonacci Number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function (n) {
+	const trib = [0, 1, 1];
+	for (let i = 3; i <= n; i++) {
+		trib[i] = trib[i - 1] + trib[i - 2] + trib[i - 3];
+	}
+	return trib[n];
+};
