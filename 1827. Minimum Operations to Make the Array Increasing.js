631b6e418edfe0975d3b6ca8f939577d5e55ff7e
Allen <jialuenyang@gmail.com>
2023-01-07 22:30:21 +0800

    Create 1827. Minimum Operations to Make the Array Increasing.js

---
+++ 1827. Minimum Operations to Make the Array Increasing.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minOperations = function (nums) {
+	let count = 0;
+	for (let i = 1; i < nums.length; i++) {
+		if (nums[i - 1] >= nums[i]) {
+			count += nums[i - 1] + 1 - nums[i];
+			nums[i] = nums[i - 1] + 1;
+		}
+	}
+	return count;
+};
