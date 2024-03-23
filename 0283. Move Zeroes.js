b2ae482780c97ab248e6caacd7465475e44d7c8d
Allen <jialuenyang@gmail.com>
2024-03-22 22:03:00 -0500

    Create 0283. Move Zeroes.js

---
+++ 0283. Move Zeroes.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+	// Use two pointers. Snowball method.
+	// One pointer will be at the first zero
+	// One pointer will be at the first non-zero after a zero
+	// Swap them (or in this case, set zero to number and number to zero)
+	let zero = 0;
+	let nonZ = 0;
+	while (true) {
+		while (nums[zero] != 0) {
+			zero++;
+			if (zero >= nums.length - 1) {
+				return;
+			}
+		}
+
+		if (nonZ < zero) {
+			nonZ = zero + 1;
+		}
+
+		while (nums[nonZ] == 0) {
+			nonZ++;
+			if (nonZ >= nums.length) {
+				return;
+			}
+		}
+
+		nums[zero] = nums[nonZ];
+		nums[nonZ] = 0;
+	}
+};
