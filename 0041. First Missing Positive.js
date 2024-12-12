58e626c0e89cdfb5490f2249d010176531a9d48c
Allen <jialuenyang@gmail.com>
2024-12-11 23:34:44 -0600

    New problems

--- 0041. First Missing Positive.js
+++ 0041. First Missing Positive.js
@@ -3,12 +3,26 @@
  * @return {number}
  */
 var firstMissingPositive = function (nums) {
-	// Turn the array of numbers into a set.
-	const set = new Set(nums);
-	// Start from 1. Keep adding 1 until the set doesn't contain it.
-	for (let i = 1; ; i++) {
-		if (!set.has(i)) {
-			return i;
+	// Swap the values in-place until it looks like [1, 2, 3....] and the missing smallest positive number will be obvious.
+	for (let i = 0; i < nums.length; i++) {
+		let num = nums[i];
+		while (num > 0 && num <= nums.length && nums[i] != nums[num - 1]) {
+			// Be careful of all edge cases. Swapping 1 with 1 will cause infinite loop without 3rd condition.
+			// Swap
+			nums[i] = nums[num - 1];
+			nums[num - 1] = num;
+
+			// Prepare for next swap
+			num = nums[i];
 		}
 	}
+
+	let i = 0;
+	while (i < nums.length) {
+		if (nums[i] != i + 1) {
+			return i + 1;
+		}
+		i++;
+	}
+	return i + 1;
 };
--- 0567. Permutation in String.js
+++ 0567. Permutation in String.js
@@ -9,14 +9,13 @@ var checkInclusion = function (s1, s2) {
 	for (const c of s1) {
 		map.set(c, (map.get(c) || 0) + 1);
 	}
-	const numOfUniqueChars = map.size;
-	let count = 0;
+	let numOfUniqueChars0 = map.size;
 
 	let r = 0;
 	while (r < s2.length) {
 		if (r >= s1.length && map.has(s2[r - s1.length])) {
 			if (map.get(s2[r - s1.length]) == 0) {
-				count--;
+				numOfUniqueChars0++;
 			}
 			map.set(s2[r - s1.length], map.get(s2[r - s1.length]) + 1);
 		}
@@ -24,8 +23,8 @@ var checkInclusion = function (s1, s2) {
 		if (map.has(s2[r])) {
 			map.set(s2[r], map.get(s2[r]) - 1);
 			if (map.get(s2[r]) == 0) {
-				count++;
-				if (count == numOfUniqueChars) {
+				numOfUniqueChars0--;
+				if (numOfUniqueChars0 == 0) {
 					return true;
 				}
 			}
